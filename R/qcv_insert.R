f27503d63b06a6cc6f76510a1d02d142b3e39e5f
Kenji T. Hayashi <kenjithayashi@gmail.com>
2023-07-11 13:13:40 -0700

    First pass at qcv_insert_list function

--- R/qcv_insert.R
+++ R/qcv_insert.R
@@ -15,7 +15,7 @@
 #' edu <- qcvexample$education
 #' qcv_insert(edu)
 #'
-#' @seealso [qcv_insert_pubs](), [qcv_insert_talks]()
+#' @seealso [qcv_insert_pubs](), [qcv_insert_talks](), [qcv_insert_list]()
 #'
 #' @export
 qcv_insert <- function(data) {
@@ -69,7 +69,7 @@ qcv_insert <- function(data) {
 #' pubs <- qcvexample$publications$articles
 #' qcv_insert_pubs(pubs)
 #'
-#' @seealso [qcv_insert](), [qcv_insert_talks]()
+#' @seealso [qcv_insert](), [qcv_insert_talks](), [qcv_insert_list]()
 #'
 #' @export
 qcv_insert_pubs <- function(data) {
@@ -124,7 +124,7 @@ qcv_insert_pubs <- function(data) {
 #' talks <- qcvexample$talks
 #' qcv_insert_talks(talks)
 #'
-#' @seealso [qcv_insert](), [qcv_insert_pubs]()
+#' @seealso [qcv_insert](), [qcv_insert_pubs](), [qcv_insert_list]()
 #'
 #' @export
 qcv_insert_talks <- function(data) {
@@ -151,6 +151,75 @@ qcv_insert_talks <- function(data) {
       d$context, ".\n",
       ":::\n",
       "\n"
+    ))
+  }
+}
+
+#' Insert a list into your CV
+#'
+#' @description `qcv_insert_list` converts a structured list (typically imported
+#' from a YAML file) into Quarto-style markdown text (including HTML/CSS
+#' where necessary) to be rendered as a CV section. Note that the code chunk in
+#' which this function is run should be given the `output: asis` option.
+#'
+#' @param data A list in the format expected by the `qcvutils` package. See
+#' `data(qcvexample)` for an example.
+#' @param type Type of list to insert. Choose from among:
+#' - `"u"`: unordered list
+#' - `"1"`: numbered list
+#' - `"a"`: alphabetical list
+#' - `"n"`: no bullets
+#'
+#' @returns Markdown text to be rendered with Quarto.
+#'
+#' @examples
+#' data(qcvexample)
+#' teaching <- qcvexample$teaching
+#' qcv_insert_list(teaching)
+#'
+#' @seealso [qcv_insert](), [qcv_insert_pubs](), [qcv_insert_talks]()
+#'
+#' @export
+qcv_insert_list <- function(data, type = "u") {
+  # Loop through each element of data
+  for (i in 1:length(data)) {
+    # Get focal element
+    d <- data[[i]]
+    # Set up spaces for details and notes
+    if (type == c("u")) {
+      spaces <- "  "
+    } else if (type %in% c("1", "a")) {
+      spaces <- "   "
+    } else if (type == "n") {
+      spaces <- ""
+    } else {
+      stop('Choose list type from among "u", "1", "a", or "n"')
+    }
+    # Generate output text
+    cat(paste0(
+      if (type == "u") {
+        "* "
+      } else if (type == "1") {
+        "1. "
+      } else if (type == "a") {
+        "a. "
+      },
+      d$title,
+      if (all(c("start", "end") %in% names(d))) {
+        paste0(" [", d$start, " - ", d$end, "]{style='float:right;'}  \n")
+      } else if ("start" %in% names(d)) {
+        paste0(" [", d$start, "]{style='float:right;'}  \n")
+      } else if ("years" %in% names(d)) {
+        paste0(" [", paste0(d$years, collapse = ", "), "]{style='float:right;'}  \n")
+      } else {
+        "  \n"
+      },
+      if ("details" %in% names(d)) {
+        paste0(spaces, d$details, "  \n")
+      },
+      if ("notes" %in% names(d)) {
+        paste0(spaces, "[", d$notes, "]{style='display:flex; color:gray; font-size:0.8em; margin: 0px auto;'}\n", collapse = "")
+      }
     ))
   }
 }
